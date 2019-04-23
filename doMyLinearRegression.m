81161092a187cca82a987543f1c9ef92498eb588
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2019-04-23 11:08:46 -0400

    added comments to doMyLinearRegression

--- doMyLinearRegression.m
+++ doMyLinearRegression.m
@@ -1,17 +1,23 @@
 function doMyLinearRegression(x,y)
-
+%% Function to create scatter plot and hold for linear regression fit
+% Returns no values
+% Two vector arguments as inputs
+% Benjamin Blouin & Christerpher Hunter
 scatter(x,y,[],y)
 
+% Formatting
 xlim([0 23])
 xticks([0:1:23])
 xlabel('Hour of Day')
 ylabel('Number of Crimes')
-
 grid on
 grid minor
 hold on;
 
+% Calling linear regression line generator
 [line]=doMyLine(x,y);
+
+% Plot return from doMyLine function
 plot(x,line)
 
 end
